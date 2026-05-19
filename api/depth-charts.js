dcec959817a5cd19936d07945cfd0fcd67d3a82f
outkizast <outkizast@gmail.com>
2026-05-18 20:30:06 -0700

    Update depth-charts.js

--- api/depth-charts.js
+++ api/depth-charts.js
@@ -5,12 +5,12 @@ const { parse } = require('node-html-parser');
 const TEAM_IDS = {
   ARI: 'ARI', ATL: 'ATL', BAL: 'BAL', BUF: 'BUF',
   CAR: 'CAR', CHI: 'CHI', CIN: 'CIN', CLE: 'CLE',
-  DAL: 'DAL', DEN: 'DEN', DET: 'DET', GB:  'GBP',
-  HOU: 'HOU', IND: 'IND', JAX: 'JAC', KC:  'KCC',
-  LAC: 'LAC', LAR: 'LAR', LV:  'LVR', MIA: 'MIA',
-  MIN: 'MIN', NE:  'NEP', NO:  'NOS', NYG: 'NYG',
+  DAL: 'DAL', DEN: 'DEN', DET: 'DET', GB:  'GB',
+  HOU: 'HOU', IND: 'IND', JAX: 'JAX', KC:  'KC',
+  LAC: 'LAC', LAR: 'LAR', LV:  'LV',  MIA: 'MIA',
+  MIN: 'MIN', NE:  'NE',  NO:  'NO',  NYG: 'NYG',
   NYJ: 'NYJ', PHI: 'PHI', PIT: 'PIT', SEA: 'SEA',
-  SF:  'SFO', TB:  'TBB', TEN: 'TEN', WAS: 'WAS',
+  SF:  'SF',  TB:  'TB',  TEN: 'TEN', WAS: 'WAS',
 };
 
 const ALL_TEAMS  = Object.keys(TEAM_IDS);
